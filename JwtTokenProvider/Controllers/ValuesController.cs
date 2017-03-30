a21c1066f32f5b1959b4939ce1a5b937705eb717
Rao Boyanapalli <boyanapalli@hotmail.com>
2017-03-30 10:21:50 -0500

    Add appsettings.json with connection string
    Add connection string and remove view models

--- JwtTokenProvider/Controllers/ValuesController.cs
+++ JwtTokenProvider/Controllers/ValuesController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JwtTokenProvider.Controllers
 {
+    [Authorize(ActiveAuthenticationSchemes = "Bearer")]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
